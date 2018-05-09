9263c95b8c4523676acd750d32330809ee5bc0a5
Eddie Moon <eddiemoonie@gmail.com>
2018-05-09 18:25:33 +0000

    cli start method

--- lib/sneaker_release/cli.rb
+++ lib/sneaker_release/cli.rb
@@ -1,10 +1,30 @@
 class SneakerRelease::CLI
 
   def call
-    #SneakerCalendar::Scraper.new.make_sneakers
+    SneakerRelease::Scraper.new.make_sneakers
     puts ""
     puts "  Welcome to the Sneaker Release Calendar"
-    #start
+    start
+  end
+
+  def start
+    #print_sneakers_list
+
+    puts ""
+    puts "Which sneaker release would you like to see?"
+    puts ""
+
+    input = gets.strip.to_i
+    if input.between?(1,12)
+      sneaker = SneakerRelease::Sneaker.find(input)
+      #print_sneaker_info(sneaker)
+    else
+      puts ""
+      puts "INVALID OPTION. Please select again from the following:"
+      start
+    end
+
+    #more_sneakers
   end
 
 end
--- lib/sneaker_release/scraper.rb
+++ lib/sneaker_release/scraper.rb
@@ -8,4 +8,10 @@ class SneakerRelease::Scraper
     self.get_page.css("div.sneaker-section.row div.release-date-image-wrapper")
   end
 
+  def make_sneakers
+    scrape_sneakers_index.each do |s|
+      SneakerRelease::Sneaker.new_from_index_page(s)
+    end
+  end
+
 end
