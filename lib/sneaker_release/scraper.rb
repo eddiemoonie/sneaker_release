765dc79c63d9225bb917e26101faa7c2851b8c72
Eddie Moon <eddiemoonie@gmail.com>
2018-05-09 18:19:38 +0000

    scraping page update

---
+++ lib/sneaker_release/scraper.rb
@@ -0,0 +1,11 @@
+class SneakerRelease::Scraper
+
+  def get_page
+    Nokogiri::HTML(open("https://www.kicksonfire.com/app/"))
+  end
+
+  def scrape_sneakers_index
+    self.get_page.css("div.sneaker-section.row div.release-date-image-wrapper")
+  end
+
+end
